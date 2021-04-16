1ab00572d6b46a560d59b92702d762240a8fd785
Carl Rosales <rosales.carlangel@gmail.com>
2021-04-16 19:23:39 +0800

    added validations for firstname and lastname: - allow_blank: false - presence: true

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :trackable
+
+  validates :firstname, allow_blank: false, presence: true
+  validates :lastname, allow_blank: false, presence: true
 end
