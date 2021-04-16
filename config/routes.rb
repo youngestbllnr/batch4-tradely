b08138ccdb6bca10384e14504e45ff423237f2b4
Carl Rosales <rosales.carlangel@gmail.com>
2021-04-16 19:41:23 +0800

    added custom configurations for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
   root 'main#index'
-  devise_for :users
+  devise_for :users, controllers: 
+                      { 
+                        sessions: 'users/sessions',
+                        registrations: 'users/registrations',
+                        confirmations: 'users/confirmations'
+                      },
+                     path: 'auth',
+                     path_names:
+                      { 
+                        sign_in: 'login',
+                        sign_out: 'logout',
+                        sign_up: 'get_started',
+                        registration: 'register',
+                        confirmation: 'verification',
+                        password: 'secret'
+                      }
 end
