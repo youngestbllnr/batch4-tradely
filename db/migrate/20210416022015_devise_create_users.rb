8d73434a2b2e22cc56cf4f2a95b3706f1ff9c2f3
Carl Rosales <rosales.carlangel@gmail.com>
2021-04-16 19:23:00 +0800

    added firstname and lastname fields to users table

--- db/migrate/20210416022015_devise_create_users.rb
+++ db/migrate/20210416022015_devise_create_users.rb
@@ -3,6 +3,9 @@
 class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
+      t.string :firstname
+      t.string :lastname
+
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
