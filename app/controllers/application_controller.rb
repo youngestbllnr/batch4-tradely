55f44c9e3718f1fdbea73e3cee9714feee95381c
Carl Rosales <rosales.carlangel@gmail.com>
2021-04-21 18:29:59 +0800

    fixed spacing on line 9, application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
     protected
 
          def configure_permitted_parameters
-              devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:firstname, :lastname, :email, :password,:role)}
+              devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:firstname, :lastname, :email, :password, :role)}
          end
 end
