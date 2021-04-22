ab43c28886a0168e764bf6998c490f191fd41caa
Carl Rosales <rosales.carlangel@gmail.com>
2021-04-22 11:44:56 +0800

    updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 2021_04_20_130258) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "role", default: 0
+    t.string "role", default: "buyer"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
