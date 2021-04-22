1f31155c4689c7ae9edb80ba2b7f5b731a0e95d7
Carl Rosales <rosales.carlangel@gmail.com>
2021-04-22 12:53:41 +0800

    optimized spacing on application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
   protected
 
-		def configure_permitted_parameters
-			devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:firstname, :lastname, :email, :password, :role)}
-		end
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:firstname, :lastname, :email, :password, :role) }
+	end
 end
