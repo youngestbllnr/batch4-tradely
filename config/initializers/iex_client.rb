ebe9553ae3f5d10f05a6148fe68a585642799fea
Carl Rosales <rosales.carlangel@gmail.com>
2021-05-04 01:47:56 +0800

    Updated configuration for IEX Client

--- config/initializers/iex_client.rb
+++ config/initializers/iex_client.rb
@@ -1,5 +1,5 @@
 IEX::Api.configure do |config|
-  config.publishable_token = Rails.application.credentials.iex[:publishable_token]
-  config.secret_token = Rails.application.credentials.iex[:secret_token]
+  config.publishable_token = ENV["IEX_PUBLISHABLE_TOKEN"]
+  config.secret_token = ENV["IEX_SECRET_TOKEN"]
   config.endpoint = 'https://cloud.iexapis.com/v1'
 end
