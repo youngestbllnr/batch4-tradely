09d694c530b5dc820987181ef83fd51f0cfc92fb
Carl Rosales <rosales.carlangel@gmail.com>
2021-05-04 19:45:39 +0800

    Setup mailer for user notifications

---
+++ app/mailers/user_notifier_mailer.rb
@@ -0,0 +1,15 @@
+class UserNotifierMailer < ApplicationMailer
+  default :from => 'carl@odonto.ph'
+
+  def send_signup_email(user)
+    @user = user
+    mail( :to => @user.email,
+    :subject => 'Thanks for signing up for our amazing app!' )
+  end
+
+  def send_approval_notification(user)
+    @user = user
+    mail( :to => @user.email,
+    :subject => 'Your broker account has been approved!' )
+  end
+end
