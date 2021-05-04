97d054ce438c32e5835aa9800cd3f1819f046e99
Carl Rosales <rosales.carlangel@gmail.com>
2021-05-04 20:40:03 +0800

    Set up transaction model and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :stocks, through: :broker_stocks
   has_many :buyers_stocks, dependent: :destroy
   has_many :stocks, through: :buyer_stocks
+  has_many :transactions
 
   enum role: { buyer: 'buyer', pending_broker: 'pending_broker', broker: 'broker', admin: 'admin' }
 
