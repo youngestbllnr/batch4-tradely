f2d49c5b7121a1c0efdbf54d8b0fd47a4d14591e
Carl Rosales <rosales.carlangel@gmail.com>
2021-05-04 20:13:19 +0800

    Initialized controllers for portfolios and transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,9 @@
 class TransactionsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :unless_broker, only: :broker
+  before_action :unless_buyer, only: :buyer
+
+  def buyer; end
+
+  def broker; end
 end
