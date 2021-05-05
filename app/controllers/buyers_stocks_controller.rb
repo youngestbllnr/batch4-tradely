3a7213b78d8f2090822f5baf0b585988c4b46101
Carl Rosales <rosales.carlangel@gmail.com>
2021-05-05 19:37:17 +0800

    Added category to transactions: to differentiate buying and selling stocks

--- app/controllers/buyers_stocks_controller.rb
+++ app/controllers/buyers_stocks_controller.rb
@@ -3,6 +3,7 @@ class BuyersStocksController < ApplicationController
   def destroy
     buyers_stock = BuyersStock.find(params[:id])
     if buyers_stock.user_id == current_user.id
+      Transaction.create(category: 'sell', user_id: buyers_stock.user.id, stock_id: buyers_stock.stock.id, broker_id: buyers_stock.broker_id, price: buyers_stock.price, units: buyers_stock.units)
       buyers_stock.destroy
       respond_to do |format|
         format.html { redirect_to buyer_portfolio_path, notice: "#{buyers_stock.stock.symbol} was successfully removed from your portfolio." }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_04_122758) do
+ActiveRecord::Schema.define(version: 2021_05_05_112936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2021_05_04_122758) do
     t.integer "broker_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category", default: "buy"
     t.index ["stock_id"], name: "index_transactions_on_stock_id"
     t.index ["user_id"], name: "index_transactions_on_user_id"
   end
