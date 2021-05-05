07d0299233239270a3259cdf3470f362ae38c59b
Carl Rosales <rosales.carlangel@gmail.com>
2021-05-05 15:40:14 +0800

    Refactored registration method on devise controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,10 +6,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def create
+    email = params[:user][:email]
+    role = params[:user][:role]
+
     # Mark broker as pending to await approval from admin
-    params[:user][:role] = 'pending_broker' if params[:user][:role] == 'broker'
-    if params[:user][:role] == 'admin' && params[:user][:email] != 'rosales.carlangel@gmail.com'
-      flash[:danger] = 'Invalid email address, this email address does not have access to an admin account.'
+    params[:user][:role] = 'pending_broker' if role == 'broker'
+
+    # Validate administrator accounts
+    if role == 'admin' && email != 'admin@tradely.com'
+      flash[:danger] = 'This email address does not have access to an admin account.'
       redirect_to root_path
     else
       super
